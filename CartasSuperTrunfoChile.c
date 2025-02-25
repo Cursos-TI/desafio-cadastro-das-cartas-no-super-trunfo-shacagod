dfa32e5577b7991be26dc0f7d086d8ae1ab0b235
shacagod <brunogombio@gmail.com>
2025-02-25 01:16:36 +0000

    começo

---
+++ CartasSuperTrunfoChile.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    char estado[20] = "São Paulo";
+    char codigocidade[20] = "A01";
+    char cidade[20] = "São Paulo";
+    int populoção = "12.400.000";
+    float aream² = "1.54";
+    float pib = "760.000.000.000";
+    int pontosturisticos = "100";
+
+
+
+
+
+    return 0
+
+
+
+
+}
